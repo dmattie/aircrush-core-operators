ffdecd720ea9fa5c3d5cdeb4031662a983d9d343
dmattie <dmattie@stfx.ca>
2021-08-31 10:14:37 -0300

    move echo to py

--- operators/echo_operator/echo_operator.py
+++ operators/echo_operator/echo_operator.py
@@ -1,24 +1,23 @@
 import datetime
 
-class Echo(BaseOperator):
+class Echo():
     
     def __init__(self,id,**kwargs):  
-
+        pass
         
     def execute(self,**kwargs):
         #Invocation INFO
         now = datetime.datetime.now()
-        print("\nInvoking operator: Echo===========")
-        print(f"Singularity Container:{self.container}")
+        print("\nInvoking operator: Echo\nStart of Echo=====================")
+        
         print(f"Current date and time: {str(now)}")
         print("Parameters passed:")
         for k in kwargs:
             print(f"\t{k}={kwargs[k]}")
         print("End of Echo=======================\n")
 
-        #Execute
-        super().pullContainer
+        
 
 if __name__ == '__main__':
-    e=Echo("mainecho", arg1="a1",arg2="a2")
-    e.execute()
+    e=Echo("mainecho")
+    e.execute(arg1="a1",arg2="a2")
