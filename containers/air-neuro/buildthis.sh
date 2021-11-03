f082596ce3ae5966d5f2c0d70664c77495194d38
Dave Mattie <dmattie@stfx.ca>
2021-11-03 18:06:01 +0000

    fsl install changes

--- containers/air-neuro/buildthis.sh
+++ containers/air-neuro/buildthis.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-#if [[ -d /tmp/air-neuro ]];then
-# rm -r /opt/local/air-neuro
-#fi
-#mkdir -p /opt/local/air-neuro
-#cp -r scripts /opt/local/air-neuro
-
-sudo singularity build  air-neuro.sif /home/vagrant/ac-client/containers/air-neuro/air-neuro.def 
+sudo singularity build --sandbox air-neuro.sif air-neuro.def 
 
 if [[ $? -eq 0 ]];then
-sudo mv air-neuro.sif /home/vagrant/singularity-containers
+#:sudo mv air-neuro.sif ~/singularity-containers
 echo run this:
 echo singularity push -U /home/vagrant/singularity-containers/air-neuro.sif library://dmattie/default/image 
 echo then update tasks
