9bcced8ac1f03baf99731c00d2c4f790f9032914
Dave Mattie <dmattie@stfx.ca>
2021-11-26 18:31:58 -0400

    support multiple scans of same modality

--- containers/air-neuro/scripts/lib/autobids/case_a.sh
+++ containers/air-neuro/scripts/lib/autobids/case_a.sh
@@ -15,17 +15,45 @@ function case_a_test {  ## ABIDE
   fi
 }
 
+# function case_a_autobids {  ##ABIDE LOOK-A-LIKE  
+#   mkdir -p $TARGET/sub-${SUBJECT}/ses-${SESSION}/anat  
+#   cp $DATASETDIR/source/$SUBJECT/session_$SESSION/anat_1/anat.nii.gz $TARGET/sub-${SUBJECT}/ses-${SESSION}/anat/sub-${SUBJECT}_ses-${SESSION}_anat.nii.gz
+#   if [[ -d $DATASETDIR/source/$SUBJECT/session_$SESSION/dti_1 ]];then
+#     mkdir -p $TARGET/sub-${SUBJECT}/ses-${SESSION}/dwi
+#     cp $DATASETDIR/source/$SUBJECT/session_$SESSION/dti_1/dti.nii.gz $TARGET/sub-${SUBJECT}/ses-${SESSION}/dwi/sub-${SUBJECT}_ses-${SESSION}_dwi.nii.gz
+#     cp $DATASETDIR/source/$SUBJECT/session_$SESSION/dti_fieldmap/dti.bvals $TARGET/sub-${SUBJECT}/ses-${SESSION}/dwi/bvals
+#     cp $DATASETDIR/source/$SUBJECT/session_$SESSION/dti_fieldmap/dti.bvecs_image $TARGET/sub-${SUBJECT}/ses-${SESSION}/dwi/bvecs
+#   fi
+#   echo "OK"
+# }
 function case_a_autobids {  ##ABIDE LOOK-A-LIKE  
   mkdir -p $TARGET/sub-${SUBJECT}/ses-${SESSION}/anat  
-  cp $DATASETDIR/source/$SUBJECT/session_$SESSION/anat_1/anat.nii.gz $TARGET/sub-${SUBJECT}/ses-${SESSION}/anat/sub-${SUBJECT}_ses-${SESSION}_anat.nii.gz
-  if [[ -d $DATASETDIR/source/$SUBJECT/session_$SESSION/dti_1 ]];then
-    mkdir -p $TARGET/sub-${SUBJECT}/ses-${SESSION}/dwi
-    cp $DATASETDIR/source/$SUBJECT/session_$SESSION/dti_1/dti.nii.gz $TARGET/sub-${SUBJECT}/ses-${SESSION}/dwi/sub-${SUBJECT}_ses-${SESSION}_dwi.nii.gz
-    cp $DATASETDIR/source/$SUBJECT/session_$SESSION/dti_fieldmap/dti.bvals $TARGET/sub-${SUBJECT}/ses-${SESSION}/dwi/bvals
-    cp $DATASETDIR/source/$SUBJECT/session_$SESSION/dti_fieldmap/dti.bvecs_image $TARGET/sub-${SUBJECT}/ses-${SESSION}/dwi/bvecs
-  fi
+  shopt -s globstar
+  for eachdir in $DATASETDIR/source/$SUBJECT/session_$SESSION/anat*;do
+    process_anat $eachdir
+  done  
+  for eachdir in $DATASETDIR/source/$SUBJECT/session_$SESSION/dti*;do
+    process_dti $eachdir
+  done    
   echo "OK"
 }
+function process_anat {
+    anatdir=$( dirname $1 )
+    SCANID=$( echo $anatdir |rev|cut -d\_ -f1 |rev )
 
+    cp $DATASETDIR/source/$SUBJECT/session_$SESSION/anat_${SCANID}/anat.nii.gz $TARGET/sub-${SUBJECT}/ses-${SESSION}/anat/sub-${SUBJECT}_ses-${SESSION}_anat_${SCANID}.nii.gz
+
+}
+function process_dti {
+
+    dtidir=$( dirname $1 )
+    SCANID=$( echo $dtidir |rev|cut -d\_ -f1 |rev )
+    mkdir -p $TARGET/sub-${SUBJECT}/ses-${SESSION}/dwi
+    cp $dtidir/dti.nii.gz $TARGET/sub-${SUBJECT}/ses-${SESSION}/dwi/sub-${SUBJECT}_ses-${SESSION}_dwi_${SCANID}.nii.gz
+    cp $DATASETDIR/source/$SUBJECT/session_$SESSION/dti_fieldmap/dti.bvals $TARGET/sub-${SUBJECT}/ses-${SESSION}/dwi/bvals
+    cp $DATASETDIR/source/$SUBJECT/session_$SESSION/dti_fieldmap/dti.bvecs_image $TARGET/sub-${SUBJECT}/ses-${SESSION}/dwi/bvecs
+
+    
+}
 export case_a_test
 export case_a_autobids
