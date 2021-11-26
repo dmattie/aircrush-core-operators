10342513964e3195cd28007a9fec4dd8cbf7d938
Dave Mattie <dmattie@stfx.ca>
2021-11-26 09:22:54 -0400

    added export

--- containers/air-neuro/scripts/lib/autobids.sh
+++ containers/air-neuro/scripts/lib/autobids.sh
@@ -6,11 +6,11 @@ source "${SCRIPTPATH}/autobids/case_a.sh"
 source "${SCRIPTPATH}/autobids/case_b.sh"
 
 
-# export case_a_test
-# export case_a_autobids
+export case_a_test
+export case_a_autobids
 
-# export case_b_test
-# export case_b_autobids
+export case_b_test
+export case_b_autobids
 
-# export case_c_test
-# export case_c_autobids
+export case_c_test
+export case_c_autobids
