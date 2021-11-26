f860760db973a6772d6619b783e884c7c4a1393c
Dave Mattie <dmattie@stfx.ca>
2021-11-26 09:29:35 -0400

    dbg

--- containers/air-neuro/scripts/lib/autobids.sh
+++ containers/air-neuro/scripts/lib/autobids.sh
@@ -2,11 +2,9 @@
 
 SCRIPT=$( realpath $0 )
 SCRIPTPATH=$( dirname $SCRIPT )
-source "${SCRIPTPATH}/autobids/case_a.sh"
-source "${SCRIPTPATH}/autobids/case_b.sh"
+source "${SCRIPTPATH}/lib/autobids/case_a.sh"
+source "${SCRIPTPATH}/lib/autobids/case_b.sh"
 
-[[ $0 != $BASH_SOURCE ]] && echo "Script is being sourced"
-echo "${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}"
 export case_a_test
 export case_a_autobids
 
