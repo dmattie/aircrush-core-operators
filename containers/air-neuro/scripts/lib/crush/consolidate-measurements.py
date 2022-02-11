6a5bc75fbdf406ce13379c35ff6de3da045df74f
Dave Mattie <dmattie@stfx.ca>
2022-02-11 18:59:42 -0400

    case sensitive var on bids

--- containers/air-neuro/scripts/lib/crush/consolidate-measurements.py
+++ containers/air-neuro/scripts/lib/crush/consolidate-measurements.py
@@ -2,11 +2,8 @@
  
 import sys,os,subprocess
 import re
-import nibabel as nib
-import numpy as np
-import warnings
-import json
 import argparse
+import json
 
 
 def process(**kwargs):#segment,counterpart,method):
@@ -16,12 +13,26 @@ def process(**kwargs):#segment,counterpart,method):
         pipeline=kwargs['pipeline']
         tidy=kwargs['tidy']
         clean=kwargs['clean']
-
+        out=kwargs['out']
         if not os.path.isdir(datasetdir):
             print(f"datasetdir not found: {datasetdir}")
             sys.exit(1)
-        for dir in os.walk(f"{datasetdir}/derivatives/{pipeline}/crush"):
-            print(dir)
+        print(f"{datasetdir}/derivatives/{pipeline}/sub-{subject}/ses-{session}/crush")
+
+        pattern = re.compile("(.*)\/(\d+)-(\d+)-(\w+)-(\w+)")
+        target = open(out,"a")
+
+        for dir in os.walk(f"{datasetdir}/derivatives/{pipeline}/sub-{subject}/ses-{session}/crush"):
+           for f in dir[2]:
+             if os.path.splitext(f"{dir[0]}/{f}")[1] == ".json":
+                try:
+                   with open(f"{dir[0]}/{f}",'r') as jq:
+                      data=json.load(jq)
+                      for k in data:
+                         keyparts=pattern.split(k)
+                         target.write(f"{pipeline},{subject},{session},{keyparts[2]},{keyparts[3]},{keyparts[4]},{keyparts[5]},{data[k]}\n")
+                except Exception as e:
+                   print(f"ERROR processing {dir[0]}/{f}\n{e}")
 
 def main():
 
@@ -35,10 +46,11 @@ def main():
     parser.add_argument('-pipeline',action='store', required=True, help="The name of the pipeline being processed to tag the data as it is stored")    
     parser.add_argument('-tidy',action='store_true', help="If specified, tar temp files into a single file" )
     parser.add_argument('-clean',action='store_true', help="If specified, remove intermediate files (.nii, .txt, .json)" )
+    parser.add_argument('-out',action='store',required=True,help='Filename for consolidated output.  Existing file will be appended')
     
     args = parser.parse_args()
 
-    process(datasetdir=args.datasetdir,subject=args.subject,session=args.session,pipeline=args.pipeline,tidy=args.tidy,clean=args.clean)
+    process(datasetdir=args.datasetdir,subject=args.subject,session=args.session,pipeline=args.pipeline,tidy=args.tidy,clean=args.clean,out=args.out)
 
 if __name__ == '__main__':
     main()
