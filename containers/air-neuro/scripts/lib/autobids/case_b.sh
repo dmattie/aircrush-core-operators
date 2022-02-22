21c91b9cab5d2fdd6a76dcbd3d81f2e76d8b529d
Dave Mattie <dmattie@stfx.ca>
2022-02-22 17:58:48 -0400

    enhancements for adnie autobids for cases with no xml

--- containers/air-neuro/scripts/lib/autobids/case_b.sh
+++ containers/air-neuro/scripts/lib/autobids/case_b.sh
@@ -22,9 +22,21 @@ function case_b_test {  ## ADNI-ii
 function case_b_autobids {  ##ADNI-ii LOOK-A-LIKE 
   #Let's do T1 first 
   shopt -s globstar
+  anat_processed=0
+
+  #There may be an xml file with metadata. Look here first
   for eachdir in $DATASETDIR/source/$SUBJECT/*MPRAGE/**/*.xml; do
     case_b_process_mprage $eachdir
+    anat_processed=1
   done
+
+  if [[ $anat_processed -eq 0 ]];then
+    #No xml file found, let's try our best:
+    for eachdir in $DATASETDIR/source/$SUBJECT/*MPRAGE/**/*_1_*.dcm; do
+      #each represents first of a series in dcm set
+      case_b_noxml_process_mprage $eachdir
+    done
+  fi
 
   for eachdcm in $DATASETDIR/source/$SUBJECT/*DTI*/**/*_1_*.dcm; do
     #each represents first of a series in dcm set
@@ -47,6 +59,14 @@ function case_b_process_mprage {
     image=$( cat $1 |grep image|cut -d\" -f2 )  
     nii=$(find $sessdir -name *.nii -print -quit 2>/dev/null | head -1)
     newsub=$(echo $SUBJECT | tr -cd '[:alnum:]')
+    mkdir -p $DATASETDIR/rawdata/sub-${newsub}/ses-${session}/anat
+    cp --no-clobber $1 $DATASETDIR/rawdata/sub-${newsub}/ses-${session}/anat/sub-${newsub}_ses-${session}_T1_$image.nii
+}
+function case_b_noxml_process_mprage {
+    sessdir=$( dirname $1 )
+    image=$( echo $1 |rev|cut -d\_ -f1 |rev|cut -d\. -f1)
+    session=$( echo $1 |rev|cut -d\_ -f2 |rev)
+    newsub=$(echo $SUBJECT | tr -cd '[:alnum:]')    
     mkdir -p $DATASETDIR/rawdata/sub-${newsub}/ses-${session}/anat
     cp --no-clobber $1 $DATASETDIR/rawdata/sub-${newsub}/ses-${session}/anat/sub-${newsub}_ses-${session}_T1_$image.nii
 }
