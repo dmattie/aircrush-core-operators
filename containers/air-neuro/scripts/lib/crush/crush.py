ee90213d9f7046cbd299e01694b57d119687b9ab
Dave Mattie <dmattie@stfx.ca>
2022-03-06 09:15:42 -0400

    .

--- containers/air-neuro/scripts/lib/crush/crush.py
+++ containers/air-neuro/scripts/lib/crush/crush.py
@@ -62,7 +62,7 @@ def process(**kwargs):#segment,counterpart,method):
    # pool = Pool(int(no_of_procs))    
     print("Multiprocessing %s tasks across %s async procs" %(len(tasks),no_of_procs))
 
-    with Pool(int(num_of_procs)) as p:
+    with Pool(int(no_of_procs)) as p:
         p.map(getmeasurements,tasks)
     # for t in tasks:
     #     print(t)
