57e584b8f2d988190e701d56abe29ffbd0e7b58f
Dave Mattie <dmattie@stfx.ca>
2022-05-07 10:50:39 -0300

    .

--- start.py
+++ start.py
@@ -296,9 +296,8 @@ def push_data(stage,project,subject,session,**kwargs):
         if stage=="derivatives":
             if "pipelines" in kwargs:
                 pipelines=kwargs['pipelines']
-                for pipeline in pipelines:
-                    print(pipeline)
-                    root=f"projects/{project.field_path_to_exam_data}/datasets/derivatives/{pipeline.field_id}/sub-{subject.title}/ses-{session.title}/"
+                for pipeline in pipelines:                    
+                    root=f"projects/{project.field_path_to_exam_data}/datasets/derivatives/{pipelines[pipeline].field_id}/sub-{subject.title}/ses-{session.title}/"
                     source_session_dir=f"{wd}/{root}"
                     target_session_dir=f"{data_transfer_node}{datacommons}/{root}"
                     print(f"Cloning ({source_session_dir}) back to data commons ({target_session_dir})")        
@@ -928,7 +927,7 @@ def cascade_status_to_subject(node_uuid):
             continue
         print(f"Synchronizing {project.title}:{subject.title}/{session.title} with status {session.field_status}")
         if session.field_status=='processed':
-            push_data("rawdata",project,subject,session)            
+            #push_data("rawdata",project,subject,session)            
             push_data("derivatives",project,subject,session,pipelines=pipelines)
             session.field_status='completed'
             session.field_responsible_compute_node=None #Free up a slot on compute node for more
