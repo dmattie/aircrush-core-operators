5328a4637f93dc3cf4af5d6a87c527061854ea4f
Dave Mattie <dmattie@stfx.ca>
2022-05-10 09:53:18 -0300

    setstatus util

---
+++ setstatus.py
@@ -0,0 +1,48 @@
+import argparse
+from pickle import FALSE
+from aircrushcore.cms import compute_node, compute_node_collection, session_collection, task_instance_collection
+from aircrushcore.controller.configuration import AircrushConfig
+from aircrushcore.dag import Workload
+from aircrushcore.cms import *
+from aircrushcore.datacommons.data_commons import DataCommons
+from util.setup import ini_settings
+
+def main():
+
+    global aircrush,crush_host,args
+    print("Aircrush set utility")
+    parser = argparse.ArgumentParser(
+        description="CRUSH client command line utility. Start all tasks with this command")
+    parser.add_argument('-set',action='store',type=str,
+        help="Specify a setting to change [ti-status].")
+    parser.add_argument('-uuid',action='store',
+        help='UUID of CMS object' )
+    parser.add_argument('-value',action='store',
+        help='New value')
+    args = parser.parse_args()
+
+    aircrush=ini_settings()
+
+    crush_host=Host(
+        endpoint=aircrush.config['REST']['endpoint'],
+        username=aircrush.config['REST']['username'],
+        password=aircrush.config['REST']['password']
+    )
+
+    if args.set == "ti-status":
+        set_ti_status(uuid=args.uuid,status=args.value)
+    else:
+        print("Nothing to do")
+
+def set_ti_status(uuid:str, status:str):
+    ti_col=TaskInstanceCollection(cms_host=crush_host)
+    if ti_col is not None:
+        ti=ti_col.get_one(uuid)
+        print(f"{ti.title}\n\tOld Status:{ti.field_status}\n\tNew Status:{status}")
+        ti.field_status=status
+        ti.upsert()
+        print("Update Complete")
+
+if __name__ == '__main__':
+    main()
+
--- start.py
+++ start.py
@@ -18,6 +18,7 @@ import subprocess
 import argparse
 import socket
 from humanfriendly import format_size, parse_size
+from util.setup import ini_settings
 
 aircrush=None
 crush_host=None
@@ -450,91 +451,91 @@ def parameter_expansion(cmdArray,parms_to_add,**kwargs):
             cmdArray.append(parm) 
    
     return cmdArray
-def ini_settings():
+# def ini_settings():
     
-    homedir=os.path.expanduser('~')
-    crush_config=f"{homedir}/.crush.ini"
-    if not os.path.isfile(crush_config):
+#     homedir=os.path.expanduser('~')
+#     crush_config=f"{homedir}/.crush.ini"
+#     if not os.path.isfile(crush_config):
         
-        settings={}
-        settings['REST']={}
-        settings['COMPUTE']={}
-        settings['COMMONS']={}
-        print(f"Looks like this is your first time here.  Let's get setup, settings will be stored in ~/.crush.ini")
+#         settings={}
+#         settings['REST']={}
+#         settings['COMPUTE']={}
+#         settings['COMMONS']={}
+#         print(f"Looks like this is your first time here.  Let's get setup, settings will be stored in ~/.crush.ini")
         
-        conf = open(crush_config, "w") 
+#         conf = open(crush_config, "w") 
 
-        settings['REST']['endpoint']=input("What is the URL of your Aircrush CMS [http://20.63.59/9/]:")
-        if settings['REST']['endpoint'] == "":
-            settings['REST']['endpoint']= "http://20.63.59.9/"
+#         settings['REST']['endpoint']=input("What is the URL of your Aircrush CMS [http://20.63.59/9/]:")
+#         if settings['REST']['endpoint'] == "":
+#             settings['REST']['endpoint']= "http://20.63.59.9/"
 
-        settings['REST']['username']=input("Aircrush username:")
-        while settings['REST']['username']=="":
-            settings['REST']['username']=input("Aircrush username:")
-        settings['REST']['password']=input("Aircrush password:")
-        while settings['REST']['password']=="":
-            settings['REST']['password']=input("Aircrush password:")
+#         settings['REST']['username']=input("Aircrush username:")
+#         while settings['REST']['username']=="":
+#             settings['REST']['username']=input("Aircrush username:")
+#         settings['REST']['password']=input("Aircrush password:")
+#         while settings['REST']['password']=="":
+#             settings['REST']['password']=input("Aircrush password:")
 
-        hostname=os.environ.get("CC_CLUSTER")
-        if hostname==None:
-            hostname=socket.gethostname()
-        settings['COMPUTE']['cluster']=input(f"Cluster name [{hostname}]")
-        if settings['COMPUTE']['cluster']=="":
-            settings['COMPUTE']['cluster']=hostname
+#         hostname=os.environ.get("CC_CLUSTER")
+#         if hostname==None:
+#             hostname=socket.gethostname()
+#         settings['COMPUTE']['cluster']=input(f"Cluster name [{hostname}]")
+#         if settings['COMPUTE']['cluster']=="":
+#             settings['COMPUTE']['cluster']=hostname
 
-        settings['COMPUTE']['account']=input("SLURM account to charge (e.g. def-username):")
-        while settings['COMPUTE']['account']=="":
-            settings['COMPUTE']['account']=input("SLURM account to charge (e.g. def-username):")
+#         settings['COMPUTE']['account']=input("SLURM account to charge (e.g. def-username):")
+#         while settings['COMPUTE']['account']=="":
+#             settings['COMPUTE']['account']=input("SLURM account to charge (e.g. def-username):")
         
-        scratch=os.environ.get("SCRATCH")
-        if scratch==None:
-            scratch="~/scratch"
-        settings['COMPUTE']['working_directory']=input(f"Working directory for scratch [{scratch}/aircrush]:")
-        if settings['COMPUTE']['working_directory']=="":
-            settings['COMPUTE']['working_directory']=f"{scratch}/aircrush"
-        os.makedirs(settings['COMPUTE']['working_directory'])
+#         scratch=os.environ.get("SCRATCH")
+#         if scratch==None:
+#             scratch="~/scratch"
+#         settings['COMPUTE']['working_directory']=input(f"Working directory for scratch [{scratch}/aircrush]:")
+#         if settings['COMPUTE']['working_directory']=="":
+#             settings['COMPUTE']['working_directory']=f"{scratch}/aircrush"
+#         os.makedirs(settings['COMPUTE']['working_directory'])
 
         
-        settings['COMPUTE']['concurrency_limit']=input("Max concurrent jobs [10]:")
-        if settings['COMPUTE']['concurrency_limit']=="":
-            settings['COMPUTE']['concurrency_limit']=10
+#         settings['COMPUTE']['concurrency_limit']=input("Max concurrent jobs [10]:")
+#         if settings['COMPUTE']['concurrency_limit']=="":
+#             settings['COMPUTE']['concurrency_limit']=10
 
-        settings['COMPUTE']['seconds_between_failures']=input("Seconds to wait between failures[18000](default 5 hrs, providing time for mitigation):")
-        if settings['COMPUTE']['seconds_between_failures']=="":
-            settings['COMPUTE']['seconds_between_failures']=18000
+#         settings['COMPUTE']['seconds_between_failures']=input("Seconds to wait between failures[18000](default 5 hrs, providing time for mitigation):")
+#         if settings['COMPUTE']['seconds_between_failures']=="":
+#             settings['COMPUTE']['seconds_between_failures']=18000
         
-        settings['COMMONS']['commons_path']=input(f"Location of data commons.  If DC is remote, provide path on that host.  (e.g. ...[HERE]/projects/project-id/datasets/source):")
-        while settings['COMMONS']['commons_path']=="":
-            print("\thint: /home/username/projects/def-username/shared/")
-            settings['COMMONS']['commons_path']=input(f"Location of data commons (e.g. ...[HERE]/projects/project-id/datasets/source):")
+#         settings['COMMONS']['commons_path']=input(f"Location of data commons.  If DC is remote, provide path on that host.  (e.g. ...[HERE]/projects/project-id/datasets/source):")
+#         while settings['COMMONS']['commons_path']=="":
+#             print("\thint: /home/username/projects/def-username/shared/")
+#             settings['COMMONS']['commons_path']=input(f"Location of data commons (e.g. ...[HERE]/projects/project-id/datasets/source):")
 
-        settings['COMMONS']['data_tranfer_node']=input(f"Data transfer node of cluster hosting data commons (leave blank if the data commons is on this cluster):")
+#         settings['COMMONS']['data_tranfer_node']=input(f"Data transfer node of cluster hosting data commons (leave blank if the data commons is on this cluster):")
         
-        settings['COMPUTE']['singularity_container_location']=input(f"Location for storing active singularity containers [{settings['COMMONS']['commons_path']}/code/containers]:")
-        if settings['COMPUTE']['singularity_container_location']=="":
-            settings['COMPUTE']['singularity_container_location']=f"{settings['COMMONS']['commons_path']}/code/containers"
-        os.makedirs(settings['COMPUTE']['singularity_container_location'])
+#         settings['COMPUTE']['singularity_container_location']=input(f"Location for storing active singularity containers [{settings['COMMONS']['commons_path']}/code/containers]:")
+#         if settings['COMPUTE']['singularity_container_location']=="":
+#             settings['COMPUTE']['singularity_container_location']=f"{settings['COMMONS']['commons_path']}/code/containers"
+#         os.makedirs(settings['COMPUTE']['singularity_container_location'])
     
-        print("Writing file")
-        L = [
-            "[REST]\n",
-            f"username={settings['REST']['username']}\n",
-            f"password={settings['REST']['password']}\n",
-            f"endpoint={settings['REST']['endpoint']}\n\n",
-            "[COMPUTE]\n",
-            f"cluster={settings['COMPUTE']['cluster']}\n",
-            f"account={settings['COMPUTE']['account']}\n",
-            f"working_directory={settings['COMPUTE']['working_directory']}\n",
-            f"concurrency_limit={settings['COMPUTE']['concurrency_limit']}\n",
-            f"seconds_between_failures={settings['COMPUTE']['seconds_between_failures']}\n"
-            f"singularity_container_location={settings['COMPUTE']['singularity_container_location']}\n\n",
-            "[COMMONS]\n",
-            f"commons_path={settings['COMMONS']['commons_path']}\n",
-            f"data_transfer_node={settings['COMMONS']['data_transfer_node']}\n"
-            ]
-        conf.writelines(L) 
-        conf.close() 
-    return AircrushConfig(crush_config)
+#         print("Writing file")
+#         L = [
+#             "[REST]\n",
+#             f"username={settings['REST']['username']}\n",
+#             f"password={settings['REST']['password']}\n",
+#             f"endpoint={settings['REST']['endpoint']}\n\n",
+#             "[COMPUTE]\n",
+#             f"cluster={settings['COMPUTE']['cluster']}\n",
+#             f"account={settings['COMPUTE']['account']}\n",
+#             f"working_directory={settings['COMPUTE']['working_directory']}\n",
+#             f"concurrency_limit={settings['COMPUTE']['concurrency_limit']}\n",
+#             f"seconds_between_failures={settings['COMPUTE']['seconds_between_failures']}\n"
+#             f"singularity_container_location={settings['COMPUTE']['singularity_container_location']}\n\n",
+#             "[COMMONS]\n",
+#             f"commons_path={settings['COMMONS']['commons_path']}\n",
+#             f"data_transfer_node={settings['COMMONS']['data_transfer_node']}\n"
+#             ]
+#         conf.writelines(L) 
+#         conf.close() 
+#     return AircrushConfig(crush_config)
 
 def getMultipliers(ti_uuid:str):
     ti_col=TaskInstanceCollection(cms_host=crush_host)
@@ -1031,7 +1032,7 @@ def cascade_status_to_subject(node_uuid):
                 pipelines[tis_for_session[ti].field_pipeline]=tis_for_session[ti].pipeline()
 
         derived_status=derive_parent_status(count_failed,count_running,count_completed,count_notstarted,count_processed,count_waiting,count_limping)
-        print(f"\tSession ({session.title}) status set to {derived_status} given the following task instance statuses:\n\t\tfailed:{count_failed} running:{count_running} completed:{count_completed} not started:{count_notstarted} processed:{count_processed} waiting:{count_waiting}")
+        print(f"\tSession ({session.title}) status set to {derived_status} given the following task instance statuses:\n\t\tfailed:{count_failed} running:{count_running} completed:{count_completed} not started:{count_notstarted} processed:{count_processed} waiting:{count_waiting} limping:{count_limping}")
         session.field_status=derived_status
         subject=session.subject()                        
 
@@ -1081,7 +1082,7 @@ def cascade_status_to_subject(node_uuid):
             count_notstarted+=1
         
         derived_status=derive_parent_status(count_failed,count_running,count_completed,count_notstarted,count_processed,count_waiting,count_limping)
-        print(f"Subject ({subjects_of_attached_sessions[subject].title}) status set to {derived_status} due to the following session statuses:\n\t\tfailed:{count_failed}, running:{count_running}, completed:{count_completed}, not started:{count_notstarted}, processed:{count_processed}, waiting: {count_waiting}")
+        print(f"Subject ({subjects_of_attached_sessions[subject].title}) status set to {derived_status} due to the following session statuses:\n\t\tfailed:{count_failed}, running:{count_running}, completed:{count_completed}, not started:{count_notstarted}, processed:{count_processed}, waiting: {count_waiting}, limping:{count_limping}")
         subjects_of_attached_sessions[subject].field_status=derived_status
         subjects_of_attached_sessions[subject].upsert()
 
