efe2190e97685aae39261894ef9df1e7f0fa5548
Dave Mattie <dmattie@stfx.ca>
2022-05-11 18:32:47 -0300

    .

--- util/conversion.py
+++ util/conversion.py
@@ -2,9 +2,20 @@ import math
 import re
 from decimal import Decimal
 
+__author__ = "Alexander Zaitsev (azaitsev@gmail.com)"
+__copyright__ = "Copyright 2018, azaitsev@gmail.com"
+__license__ = "MIT"
+__version__ = "0.1.1"
+
+
+def remove_exponent(d):
+    """Remove exponent."""
+    return d.quantize(Decimal(1)) if d == d.to_integral() else d.normalize()
+
+
 def millify(n, precision=0, drop_nulls=True, prefixes=[]):
     """Humanize number."""
-    millnames = ['', 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y']
+    millnames = ['', 'k', 'M', 'B', 'T', 'P', 'E', 'Z', 'Y']
     if prefixes:
         millnames = ['']
         millnames.extend(prefixes)
@@ -15,3 +26,13 @@ def millify(n, precision=0, drop_nulls=True, prefixes=[]):
     if drop_nulls:
         result = remove_exponent(Decimal(result))
     return '{0}{dx}'.format(result, dx=millnames[millidx])
+
+
+def prettify(amount, separator=','):
+    """Separate with predefined separator."""
+    orig = str(amount)
+    new = re.sub("^(-?\d+)(\d{3})", "\g<1>{0}\g<2>".format(separator), str(amount))
+    if orig == new:
+        return new
+    else:
+        return prettify(new)
