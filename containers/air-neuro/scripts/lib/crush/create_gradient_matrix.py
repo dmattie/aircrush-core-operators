c9c476df04de52cc8f6734609ac0af851e1a55f2
Dave Mattie <dmattie@stfx.ca>
2022-05-11 09:00:00 -0300

    fix gradient directions check

--- containers/air-neuro/scripts/lib/crush/create_gradient_matrix.py
+++ containers/air-neuro/scripts/lib/crush/create_gradient_matrix.py
@@ -38,7 +38,7 @@ else:
     transposed_csv.to_csv(args.out,header=False,index=False)
 print("Creating number of measurement points datafile")
 f = open(f"{args.out}.directions", "a")
-f.write(transposed_csv.shape[0])
+f.write(f"{transposed_csv.shape[0]}")
 f.close()
 
         
