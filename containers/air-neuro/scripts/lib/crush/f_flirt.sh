62e5754026e463d8fd1e4fcd23a1c260cf737117
Dave Mattie <dmattie@stfx.ca>
2022-05-15 19:01:09 -0300

    unzipped reg2brain

--- containers/air-neuro/scripts/lib/crush/f_flirt.sh
+++ containers/air-neuro/scripts/lib/crush/f_flirt.sh
@@ -13,17 +13,21 @@ function f_flirt()
     fi
     echo "Calculating RegTransform4D"
 
-    if [[ ! -f $TARGET/reg2brain.data.nii.gz ]];then
-        echo "flirt:unable to find input file[$TARGET/reg2brain.data.nii.gz]."     
-        return 1   
-    fi
+    if [[ -f $TARGET/reg2brain.data.nii ]];then
+        reg2brain=$TARGET/reg2brain.data.nii
+    else
+        if [[ ! -f $TARGET/reg2brain.data.nii.gz ]];then
+            echo "flirt:unable to find input file[$TARGET/reg2brain.data.nii.gz]."     
+            return 1   
+        fi
+        reg2brain=$TARGET/reg2brain.data.nii.gz
 
     if [[ ! -f $FREESURFER/mri/brainmask.nii ]];then
         echo "flirt:unable to find reference brainmask.nii[$FREESURFER/mri/brainmask.nii]."     
         return 1   
     fi
     #/usr/bin/time -v flirt -in $TARGET/reg2brain.data.nii.gz -ref $FREESURFER/mri/brainmask.nii -omat $TARGET/RegTransform4D
-    flirt -in $TARGET/reg2brain.data.nii.gz -ref $FREESURFER/mri/brainmask.nii -omat $TARGET/RegTransform4D
+    flirt -in $reg2brain -ref $FREESURFER/mri/brainmask.nii -omat $TARGET/RegTransform4D
     ret=$?
     if [[ $ret -ne 0 ]];then
         >&2 echo "ERROR: flirt failed with error. see above."
