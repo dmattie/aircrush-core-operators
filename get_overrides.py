285b3dc8e9429c50da694cf8eecb7f3678ce1ac6
Dave Mattie <dmattie@stfx.ca>
2022-05-17 09:09:25 -0300

    convert failed to terminal if indicated on the project

--- get_overrides.py
+++ get_overrides.py
@@ -11,8 +11,9 @@ crush_host=Host(
 
 p_col=ProjectCollection(cms_host=crush_host)
 #p=p_col.get_one(uuid='15f20818-7f27-4b94-81de-df1630e82982')
-p=p_col.get_one(uuid='15f20818-7f27-4b94-81de-df1630e82982')
+p=p_col.get_one(uuid='5cb83f87-7869-452c-b8c7-373d7cceff31')
 
 overrides=p.get_overrides(task_uuid="ccc1a993-b3ae-4638-af5c-b4d651ba42bb")
 
 print(overrides)
+print(f"treat failed as terminal:{p.field_treat_failed_as_terminal}")
