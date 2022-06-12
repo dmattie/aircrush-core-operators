3fc7f36bf9eaabafac1f22d0af47d7e9e02e7f88
Dave Mattie <dmattie@stfx.ca>
2022-06-12 18:44:33 -0300

    .

--- make-dataframe.py
+++ make-dataframe.py
@@ -7,7 +7,7 @@ from aircrushcore.cms.project_collection import ProjectCollection
 from aircrushcore.cms.subject_collection import SubjectCollection
 from aircrushcore.cms.session_collection import SessionCollection
 from aircrushcore.controller.configuration import AircrushConfig
-import os
+import os,sys
 from multiprocessing import Pool,cpu_count
 import tarfile
 import uuid
@@ -70,23 +70,18 @@ def main():
     global crush_host
     parser = argparse.ArgumentParser(
         description="Build dataframe of CRUSH results")
-    parser.add_argument('--project',action='store',type=str,
-        help="Specify Project Name to consolidate", required=True)
+    parser.add_argument('--infile',action='store',type=str,
+        help="Specify CSV containing list of completed sessions (project,subject,session).  E.g. adni,014S6437,S965218", required=True)
     parser.add_argument('--out',action='store',type=str,
-        help="output filename to create", required=True)
+        help="output filename to create dataframe", required=True)
     args = parser.parse_args()
 
     if os.path.exists(args.out):
         print(f"Target already exists ({args.out})")
         return
-    ########## Get connected ##########
 
     aircrush=AircrushConfig()
-    crush_host=Host(
-        endpoint=aircrush.config['REST']['endpoint'],
-        username=aircrush.config['REST']['username'],
-        password=aircrush.config['REST']['password']
-    )
+
     if aircrush.get_config_dtn()!="":
         print(f"The data commons appears to be stored on another cluster according to crush.ini COMMONS/data_transfer_node setting ({aircrush.get_config_dtn()}).  This code will only work on the cluster that houses the data commons.")
         return
@@ -96,30 +91,18 @@ def main():
     
     ######## Get Sessions #########
     to_process=[]
-    proj_coll=ProjectCollection(cms_host=crush_host)
-    project=proj_coll.get_one_by_name(args.project)
-    if project is not None:
-        sess_coll=SessionCollection(cms_host=crush_host)
-        print(f"Seeking sessions for project {project.uuid}")
-        sessions=sess_coll.get(filter=f"&filter[proj-filter][condition][path]=field_participant.field_project.id&filter[proj-filter][condition][operator]=%3D&filter[proj-filter][condition][value]={project.uuid}")
-        if sessions is None:
-            print(f"No sessions found")
-            return
-        cnt_completed=0
-        cnt_total=len(sessions)
-        for session in sessions:            
-            if sessions[session].field_status=='completed':
-                cnt_completed=cnt_completed+1
-                to_process.append([session,crush_host,aircrush.get_config_datacommons(),working_dir,args.out])
-
-        print(f"Total sessions: {cnt_total}, completed: {cnt_completed} ({cnt_completed/cnt_total*100}%)")
-    else:
-        print(f"Project not found {args.project}")
+    infile=args.infile
+    f = open(infile, "r")
+    for ses in f:
+        line=ses.split(',')
+        to_process.append(line)
 
     if len(to_process)==0:
         print(f"No completed sessions found for {args.project}")
         return
-    
+    for todo in to_process:
+        print(todo[1])
+    sys.exit(1)
     ################
     # Get Tracts
     ################
