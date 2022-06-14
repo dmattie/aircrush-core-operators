65c3546b5de65da7fa0270d23f5cc72997883042
Dave Mattie <dmattie@stfx.ca>
2022-06-14 11:24:54 -0300

    .

--- data_exists.sh
+++ data_exists.sh
@@ -4,13 +4,13 @@ subject=$2
 session=$3
 
 
-if [[ ! -d ~/scratch/datacommons/projects/$project/datasets/rawdata/sub-${subject}/ses-${session} ]];then
-    echo "Missing ~/scratch/datacommons/projects/$project/datasets/rawdata/sub-${subject}/ses-${session}"
+if [[ -d ~/scratch/datacommons/projects/$project/datasets/rawdata/sub-${subject}/ses-${session} || -f ~/scratch/datacommons/projects/$project/datasets/rawdata/sub-${subject}/sub-${subject}_ses-${session}.tar.deleteme ]];then
+    echo "Found ~/scratch/datacommons/projects/$project/datasets/rawdata/sub-${subject}/ses-${session}"
 fi 
-if [[ ! -d ~/scratch/datacommons/projects/$project/datasets/derivatives/levman/sub-${subject}/ses-${session} ]];then
-    echo "Missing ~/scratch/datacommons/projects/$project/datasets/derivatives/levman/sub-${subject}/ses-${session}"
+if [[  -d ~/scratch/datacommons/projects/$project/datasets/derivatives/levman/sub-${subject}/ses-${session} || -f ~/scratch/datacommons/projects/$project/datasets/derivatives/levman/sub-${subject}/sub-${subject}_ses-${session}.tar.deleteme ]];then
+    echo "Found ~/scratch/datacommons/projects/$project/datasets/derivatives/levman/sub-${subject}/ses-${session}"
 fi
-if [[ ! -d ~/scratch/datacommons/projects/$project/datasets/derivatives/freesurfer/sub-${subject}/ses-${session} ]];then
-    echo "Missing ~/scratch/datacommons/projects/$project/datasets/derivatives/freesurfer/sub-${subject}/ses-${session}"
+if [[ -d ~/scratch/datacommons/projects/$project/datasets/derivatives/freesurfer/sub-${subject}/ses-${session} || -f ~/scratch/datacommons/projects/$project/datasets/derivatives/freesurfer/sub-${subject}/sub-${subject}_ses-${session}.tar.deleteme ]];then
+    echo "Found ~/scratch/datacommons/projects/$project/datasets/derivatives/freesurfer/sub-${subject}/ses-${session}"
 fi
 
