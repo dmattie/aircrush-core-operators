cc3c9c1570be8b9046ecc5415855b89c2eef5666
Dave Mattie <dmattie@stfx.ca>
2022-07-07 08:05:31 -0300

    sync back seems to be broken, temp script to repair

---
+++ resync-back-to-datacommons.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+project=$1
+subject=$2
+session=$3
+remote=$4
+
+echo "Re-committing $project/${subject}/${session}"
+
+fn="sub-${subject}/sub-${subject}_ses-${session}.tar"
+source='~/scratch/datacommons/projects/$project/datasets/derivatives/'
+dest='~/projects/rrg-jlevman/shared/projects/$project/datasets/derivatives/'
+if [[ -f $source/levman/$fn ]];then
+    if [[ $remote == "" ]];then
+        echo mkdir -p $dest/levman/sub-${subject}
+        echo rsync $source/levman/$fn $dest/levman/$fn
+    fi
+fi
