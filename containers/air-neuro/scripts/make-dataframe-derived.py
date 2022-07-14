f0cbb5acafb28e2fea076e26d3cc1f374ecb4eab
Dave Mattie <dmattie@stfx.ca>
2022-07-14 09:11:17 -0300

    moved derivations to separate extract due to differences in load

--- containers/air-neuro/scripts/make-dataframe-derived.py
+++ containers/air-neuro/scripts/make-dataframe-derived.py
@@ -148,147 +148,25 @@ def add_derived_measurements(dataframe_filename:str, segmentmap_filename:str):
         
     
 
-def getmeasurements(task_details):
-    print(task_details)
-    project=task_details[0]
-    subject=task_details[1]
-    session=task_details[2]
-    dc=task_details[3]
-    target=task_details[4]  
-    
-    key=f"{subject}/{session}"
-    
-    file=f"{dc}/projects/{project}/datasets/derivatives/levman/sub-{subject}/sub-{subject}_ses-{session}.tar"
-    if not os.path.isfile(file):
-        print(f"File not found ({file})")
-        return
-    else:
-        try:
-            tar = tarfile.open(file)
-        except Exception as e:
-            print(f"Error opening tarfile {file}\n{e}")
-            return
-
-        parentdir=os.path.dirname(file)
-        os.makedirs(parentdir,exist_ok=True)
-
-           
-        important_files = [  tarinfo for tarinfo in tar.getmembers()
-                if tarinfo.name.startswith(f"ses-{session}/crush.txt")
-                ]
-        
-        tar.extractall(f"{target}/sub-{subject}",important_files)
-        
-        crushfile=f"{target}/sub-{subject}/ses-{session}/crush.txt"  
-        print(f"crushfile:{crushfile}")      
-        return crushfile
-    return None
-
-            
-def tmpdir(working_dir):
-    
-    found=False
-    while found==False:
-        dir=f"{working_dir}/temp_{str(uuid.uuid4())[:8]}"        
-        if not os.path.isdir(dir):            
-            try:
-                os.makedirs(dir)
-                found=True
-            except OSError as e:
-                if e.errno != errno.EEXIST:
-                    raise
-    return dir
+       
 def main():
     global crush_host
     parser = argparse.ArgumentParser(
-        description="Build dataframe of CRUSH results")
-    parser.add_argument('--infile',action='store',type=str,
-        help="Specify CSV containing list of completed sessions (project,subject,session).  E.g. adni,014S6437,S965218", required=True)
-    parser.add_argument('--out',action='store',type=str,
-        help="output filename to create dataframe", required=True)
-    parser.add_argument('--datacommons',action='store',type=str,help='Directory to datacommons')
-    parser.add_argument('--workingdir',action='store',type=str,help='Directory to workspace')
-    parser.add_argument('--segmentmap',action='store',type=str,help='Filename Location of segment map')
+        description="Dataframe of CRUSH results")
+    parser.add_argument('--df',action='store',type=str,
+        help="crush dataframe file", required=True)    
+    parser.add_argument('--segmentmap',action='store',type=str,help='Filename Location of segment map')    
     parser.add_argument('--overwrite',action='store_true',help='Overwrite destination if it exists')
     args = parser.parse_args()
 
-    if os.path.exists(args.out):
+    if os.path.exists(args.df):
         if args.overwrite:
-            os.remove(args.out)
+            os.remove(args.df)
         else:
-            print(f"Target already exists ({args.out})")
-            return
-
-    #aircrush=AircrushConfig()
-
-    # if aircrush.get_config_dtn()!="":
-    #     print(f"The data commons appears to be stored on another cluster according to crush.ini COMMONS/data_transfer_node setting ({aircrush.get_config_dtn()}).  This code will only work on the cluster that houses the data commons.")
-    #     return
-    dc=args.datacommons#aircrush.get_config_datacommons()
-
-    working_dir=tmpdir(args.workingdir)#aircrush.get_config_wd())
-    print(f"Staging directory: {working_dir}")
+            print(f"Target already exists ({args.df})")
+            return    
     
-    ######## Get Sessions #########
-    to_process=[]
-    infile=args.infile
-    f = open(infile, "r")
-    cnt=0
-    for ses in f:
-        line=ses.strip('\n').split(',')
-        to_process.append(line)
-        cnt=cnt+1
-        # if cnt>2:
-        #     break
-
-    if len(to_process)==0:
-        print(f"No completed sessions found for {args.project}")
-        return
-    makethese=[]
-    for todo in to_process:
-        print(todo[1])
-        makethese.append([todo[0],todo[1],todo[2],dc,working_dir])
-    
-    ################
-    # Get Tracts
-    ################
-
-    no_of_procs = os.getenv("SLURM_CPUS_PER_TASK")#cpu_count()    
-    if no_of_procs is None:
-        no_of_procs=1
-
-    print(f"Multiprocessing {len(to_process)} tasks across {no_of_procs} CPUs")
-
-    #with Pool(int(no_of_procs)) as p:
-    #    p.map(getmeasurements,to_process)
-    results = Pool(int(no_of_procs)).map(getmeasurements,makethese)
-
-    crushfiles = [result for result in results]
-    
-    ################ APPEND TO DATAFRAME #################
-    print(f"Results:\n---------n\{results}\n------------\n")
-    with open(args.out,"wb") as fout:
-        if crushfiles is None:
-            print("No crush files found in completed samples")
-            sys.exit(1)
-        missing_count=0
-        for crushf in crushfiles:
-            if crushf is not None:
-                if os.path.exists(crushf):
-                    with open(crushf, "rb") as f:            
-                        fout.write(f.read())
-                else:
-                    print(f"Missing file:{crushf}")
-            else:
-                missing_count=missing_count+1
-        if missing_count>0:
-            print(f"{missing_count} of the believed to be completed sessions did not have a crush.txt file")
-
-        
-    ################ CLEANUP ############################
-    shutil.rmtree(working_dir)
-
-    #add_derived_measurements(args.out,args.segmentmap)
+    add_derived_measurements(args.df,args.segmentmap)
     
 
 
